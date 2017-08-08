c4247164d2191fe110ce3bc426cc0e1aef953a2f
Haaxor1689 <betkomaros@gmail.com>
2017-08-08 10:28:13 +0200

    Initial commit

---
+++ DerivedData/MultiPing/Build/Intermediates/MultiPing.build/Debug-iphoneos/MultiPing.build/DerivedSources/MultiPing_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char MultiPingVersionString[];
+ extern const double MultiPingVersionNumber;
+
+ const unsigned char MultiPingVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:MultiPing  PROJECT:MultiPing-1" "\n";
+ const double MultiPingVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ DerivedData/MultiPing/Build/Intermediates/Pods.build/Debug-iphoneos/Pods-MultiPing.build/DerivedSources/Pods_MultiPing_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_MultiPingVersionString[];
+ extern const double Pods_MultiPingVersionNumber;
+
+ const unsigned char Pods_MultiPingVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_MultiPing  PROJECT:Pods-1" "\n";
+ const double Pods_MultiPingVersionNumber __attribute__ ((used)) = (double)1.;
